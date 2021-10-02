1f22d27778d39b8eab12c76fc554fb07a2cbee0c
HEEJAEKIM <kkaner@linecorp.com>
2021-10-02 12:58:15 +0900

    Initial Commit

---
+++ Shared/SandwichesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichesApp.swift
+//  Shared
+//
+//  Created by Heejae Kim on 2021/10/02.
+//
+
+import SwiftUI
+
+@main
+struct SandwichesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
