618ca835956bf13873258bdbf3498ea9683d322a
HEEJAEKIM <kkaner@linecorp.com>
2021-10-02 15:32:51 +0900

    Add, Move, Remove

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -16,6 +16,9 @@ struct ContentView: View {
                 ForEach(store.sandwiches) { sandwich in
                     SandwichCell(sandwich: sandwich)
                 }
+                .onMove(perform: moveSandwiches)
+                .onDelete(perform: deleteSandwiches)
+                
                 HStack {
                     Spacer()
                     Text("\(store.sandwiches.count) Sandwiches")
@@ -24,6 +27,31 @@ struct ContentView: View {
                 }
             }
             .navigationTitle("Snadwiches")
+            .toolbar {
+                ToolbarItemGroup(placement: .bottomBar) {
+                    Button("Add", action: makeSandwich)
+                    EditButton()
+                }
+            }
+        }
+    }
+    
+    func makeSandwich() {
+        withAnimation {
+            store.sandwiches.append(Sandwich(name: "Patty Melt",
+                                             ingredientCount: 3))
+        }
+    }
+
+    func moveSandwiches(from: IndexSet, to: Int) {
+        withAnimation {
+            store.sandwiches.move(fromOffsets: from, toOffset: to)
+        }
+    }
+
+    func deleteSandwiches(offsets: IndexSet) {
+        withAnimation {
+            store.sandwiches.remove(atOffsets: offsets)
         }
     }
 }
