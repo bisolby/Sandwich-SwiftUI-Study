bc51c851a09d28a30136991012fc493fbbd7dbe8
HEEJAEKIM <kkaner@linecorp.com>
2021-10-02 14:01:28 +0900

    Extract subview

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -13,18 +13,7 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List(sandwiches) { sandwich in
-                NavigationLink(destination: Text(sandwich.name)) {
-                    HStack {
-                        Image(sandwich.thumbnailName)
-                            .cornerRadius(8)
-                        VStack(alignment: .leading) {
-                            Text(sandwich.name)
-                            Text("\(sandwich.ingredientCount) ingredients")
-                                .font(.subheadline)
-                                .foregroundColor(.secondary)
-                        }
-                    }
-                }
+                SandwichCell(sandwich: sandwich)
             }
             .navigationTitle("Snadwiches")
         }
@@ -34,5 +23,24 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView(sandwiches: testData)
+    }
+}
+
+struct SandwichCell: View {
+    var sandwich: Sandwich
+    
+    var body: some View {
+        NavigationLink(destination: Text(sandwich.name)) {
+            HStack {
+                Image(sandwich.thumbnailName)
+                    .cornerRadius(8)
+                VStack(alignment: .leading) {
+                    Text(sandwich.name)
+                    Text("\(sandwich.ingredientCount) ingredients")
+                        .font(.subheadline)
+                        .foregroundColor(.secondary)
+                }
+            }
+        }
     }
 }
