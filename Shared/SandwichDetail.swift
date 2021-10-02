952ff9c0742af8e34a750d78afe5d92a0aa0ce73
HEEJAEKIM <kkaner@linecorp.com>
2021-10-02 14:42:36 +0900

    @State

--- Shared/SandwichDetail.swift
+++ Shared/SandwichDetail.swift
@@ -9,11 +9,15 @@ import SwiftUI
 
 struct SandwichDetail: View {
     var sandwich: Sandwich
+    @State private var zoomed = false
     
     var body: some View {
         Image(sandwich.imageName)
             .resizable()
-            .aspectRatio(contentMode: .fit)
+            .aspectRatio(contentMode: zoomed ? .fill : .fit)
+            .onTapGesture {
+                zoomed.toggle()
+            }
             .navigationTitle(sandwich.name)
     }
 }
